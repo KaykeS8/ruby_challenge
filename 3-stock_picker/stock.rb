25c8a75e99e0a5de4e2c9971c854e9aac8f2144f
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2023-07-29 18:08:42 -0300

    Desafio concluido com sucesso

---
+++ 3-stock_picker/stock.rb
@@ -0,0 +1,20 @@
+def stock_picker(prices)
+  lowest = prices[0]
+  profit = 0
+  lowest_index = 0
+  result = []
+  prices.each_with_index do |price, index|
+    if price < lowest
+      lowest = price
+      lowest_index = index
+    end
+
+    if price - lowest > profit
+      profit = price - lowest
+      result = [lowest_index, index]
+    end
+  end
+  result
+end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
