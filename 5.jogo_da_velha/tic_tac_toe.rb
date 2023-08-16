296eded494b5ab8dd407e7ac97de4d9cac305c5f
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2023-08-16 20:43:40 -0300

    Implementação do metodo print_board, responsavel pela criação do tabuleiro, refatoração do metodo play responsavel por iniciar o jogo

--- 5.jogo_da_velha/tic_tac_toe.rb
+++ 5.jogo_da_velha/tic_tac_toe.rb
@@ -1,60 +1,73 @@
 module TicTacToe
+  WIN_COMBINATION = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
 
   class Game
 
-    def initialize(player_1_class, player_2_class)
-      @players = [player_1_class, player_2_class]
-      @board = []
+    attr_accessor :players
+    attr_reader :players, :available_positions
+    def initialize()
+      @players = []
+      @board = %w[1 2 3 4 5 6 7 8 9]
+      @available_positions = [1,2,3,4,5,6,7,8,9]
     end
 
-    def self.play 
-      puts "O jogo iniciou"
-      print "Digite seu nome player-1: "
-      @player_name_1 = gets.chomp
-      print "Digite seu nome player-2:"
-      @player_name_2 = gets.chomp
+    def play 
+      puts "Seje bem vindo ao jogo da velha. Preencha as informações abaixo para iniciar o game ↓"
+      # sleep(1.4)
+        print "Digite seu nome player-1: "
+        @player_name_1 = gets.chomp.strip
+        print "Digite seu nome player-2:"
+        @player_name_2 = gets.chomp.strip
 
-      player1 = PlayerHuman.new(@player_name_1, 'X')
-      player2 = PlayerHuman.new(@player_name_2, 'O')
-      Game.new(player1, player2)
+        player1 = PlayerHuman.new(@player_name_1, self, 'X')
+        player2 = PlayerHuman.new(@player_name_2, self,'O')
+        self.players = [player1, player2]
+
+        puts "Você tem jogadas de 1 a 9"
+        print_board
     end
 
+    def print_board
+      divider = "--+---+--"
+      puts "\n#{@board[0]} | #{@board[1]} | #{@board[2]}"
+      print divider
+      puts "\n#{@board[3]} | #{@board[4]} | #{@board[5]}"
+      print divider
+      puts "\n#{@board[6]} | #{@board[7]} | #{@board[8]}"
+    end
+
+    def win?
+
+    end
 
   end
 
   class Player
-    def initialize(name, marker)
+    attr_reader :name, :marker
+    def initialize(name, game_class, marker)
       @name = name
       @marker = marker
+      @game_class = game_class
     end
   end
 
   class PlayerHuman < Player
+    def choose_position
+      loop do
+        print "Digite uma posição valida: "
+        position_chosen = gets.chomp.to_i
+        if @game_class.available_positions.include?(position_chosen)
+          return position_chosen
+        end
+        puts "\nEntrada invalida"
+      end
+    end
   end
 
 end
 
 include TicTacToe
-# Game.play
+tic_tac = Game.new
+tic_tac.play
 
-puts "
-   1 | 2 | 3
-  ---+---+---
-   4 | X | 6 
-  ---+---+---
-   7 | 8 | 9 
-   "
-
-   puts rand(9).ceil
-
-=begin
-Criar o board a tabela 3x3
-Permitir o player fazer uma jogada, e com essa jogada preencher o marker no board
-Exemplo :)
-   1 | 2 | O
-  ---+---+---
-   4 | X | 6 
-  ---+---+---
-   7 | 8 | 9 
-
-=end
+# https://replit.com/@rreiso/TicTacToe-Ruby#main.rb
