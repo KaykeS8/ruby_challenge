4a760337afea28fe14e2e67b850266ee27aca76d
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2023-08-16 12:08:43 -0300

    Criação da class Player e PlayerHuman

--- 5.jogo_da_velha/tic_tac_toe.rb
+++ 5.jogo_da_velha/tic_tac_toe.rb
@@ -1,34 +1,60 @@
 module TicTacToe
 
-  LINES = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
   class Game
+
     def initialize(player_1_class, player_2_class)
-      @board = Array.new(10)
-      @current_player_id = 0
-      @Players = [player_1_class.new(self, "X"), player_2_class.new(self, "O")]
+      @players = [player_1_class, player_2_class]
+      @board = []
+    end
+
+    def self.play 
+      puts "O jogo iniciou"
+      print "Digite seu nome player-1: "
+      @player_name_1 = gets.chomp
+      print "Digite seu nome player-2:"
+      @player_name_2 = gets.chomp
+
+      player1 = PlayerHuman.new(@player_name_1, 'X')
+      player2 = PlayerHuman.new(@player_name_2, 'O')
+      Game.new(player1, player2)
+    end
+
+
+  end
+
+  class Player
+    def initialize(name, marker)
+      @name = name
+      @marker = marker
     end
   end
 
-
-  def free_position
-    (1..9).select {|position| @board[position].nil?}
+  class PlayerHuman < Player
   end
-end
 
-
-class Player
-  def initialize(name, marker)
-    @name = name
-    @marker = marker
-  end
-  attr_reader :marker
-end
-
-class HumanPlayer < Player
-end
-
-class ComputerPlayer < Player
 end
 
 include TicTacToe
-p Game.new(HumanPlayer, ComputerPlayer).free_position
+# Game.play
+
+puts "
+   1 | 2 | 3
+  ---+---+---
+   4 | X | 6 
+  ---+---+---
+   7 | 8 | 9 
+   "
+
+   puts rand(9).ceil
+
+=begin
+Criar o board a tabela 3x3
+Permitir o player fazer uma jogada, e com essa jogada preencher o marker no board
+Exemplo :)
+   1 | 2 | O
+  ---+---+---
+   4 | X | 6 
+  ---+---+---
+   7 | 8 | 9 
+
+=end
