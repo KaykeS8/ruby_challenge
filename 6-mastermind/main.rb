0dabcd10e4ee8910498a0350512ccdeafed778ce
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2023-08-27 21:38:12 -0300

    Introdução do game

---
+++ 6-mastermind/main.rb
@@ -0,0 +1,17 @@
+def intro
+  puts <<-Intro
+    Seja bem vindo ao jogo MasterMind
+
+    Mastermind é um jogo de quebra de código para dois jogadores.
+
+    Nesta versão é você contra o computador - você escolhe se quer ser
+    code-breaker ou code-maker.
+
+    O decifrador tem 12 tentativas para adivinhar o código definido pelo criador do código.
+
+    Boa sorte!! Você vai precisar... O computador é bom!
+
+   Intro
+end
+
+# https://replit.com/@agentlemanthief/mastermind#main.rb
