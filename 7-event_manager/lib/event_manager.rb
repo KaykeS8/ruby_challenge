de55d5cfecf28e371e14490ff755d2dc21a5eda5
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2023-09-08 12:13:07 -0300

    Event manager

--- 7-event_manager/lib/event_manager.rb
+++ 7-event_manager/lib/event_manager.rb
@@ -35,19 +35,18 @@ end
 
 def clean_home_phone(phone_number)
   phone = phone_number.gsub(/\D/, '')
-  case phone
-  when phone.length < 10
-    puts "Number incorrect"
-  when phone.length == 11 && phone[0] == '1'
-    phone = phone[1..-1]
-    puts "Number correct: #{phone}"
-  when phone.length == 11 && phone[0] != '1'
-    puts "Number incorrect"
-  when phone.length > 11
-    puts "Number incorrect"
+  if phone.length == 10
+    phone
+  elsif phone.length == 11 && phone[0] == '1'
+    phone[1..10]
   else
-    puts "Correct number: #{phone}"
+    "Wrong Number"
   end
+end
+
+
+def count_frequency(array)
+  array.max_by {|a| array.count(a)}
 end
 
 contents = CSV.open(
@@ -58,18 +57,30 @@ contents = CSV.open(
 
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
+contents_size = CSV.read(arquivo).length
+contents_size-=1
+date_hours = Array.new(contents_size)
+date_of_week = Array.new(contents_size)
+j = 0
+cal = {0=>"sunday",1=>"monday",2=>"tuesday",3=>"wednesday",4=>"thursday",5=>"friday",6=>"saturday"}
 
 contents.each do |row|
   name = row[:first_name]
-
-
   id = row[0]
-  zipcode = clean_zipcode(row[:zipcode])
+  reg_date = row[:regdate].gsub('/', '-')
+  reg_date_to_print =  DateTime.strptime(reg_date, '%m-%d-%y %H:%M')
+  date_hours[j] =  reg_date_to_print.hour
+  date_of_week[j] = reg_date_to_print.wday
+  j+= 1
+  
   phone_number = clean_home_phone(row[:homephone])
-
+  zipcode = clean_zipcode(row[:zipcode])
   legislators = legislator_by_zipcode(zipcode)
   form_letter = erb_template.result(binding)
 
   save_thank_you_letter(id, form_letter)
   
 end
+
+puts "A hours is more repeat is: #{count_frequency(date_hours)}"
+puts "A week day is more repeat is: #{cal[count_frequency(date_of_week)]}"
